6093d4936b594e23aa7c82569bd8a2dc10ebdc70
Noman Jatala <cctv2093@gmail.com>
2025-08-12 20:22:32 +0500

    ok

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -6,11 +6,12 @@ function App() {
 
   return (
     <>
-      <div className='text-white  bg-fuchsia-500 border border-amber-600 h-screen flex flex-col justify-around items-center'>
-        <div className='mb-6 text-slate-800 text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold'>Counter with <code className='text-blue-500 '>React</code> ,<code className='text-blue-500 '>TailwindCSS</code> and <code className='text-blue-500 '>vite</code>
+      <div className='text-white  bg-amber-200 border border-amber-600 h-screen flex flex-col justify-center items-center'>
+        <div className=' mb-7 text-slate-800 text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold'>Counter with
         </div>
+         <span className='text-blue-500 text-2xl mb-7 sm:text-3xl md:text-4xl lg:text-5xl font-bold'>React, TailwindCSS, Vite</span>
         <div
-          className='bg-slate-600 rounded-3xl h-1/3 w-2/3 flex justify-center items-center'>
+          className='bg-slate-600 rounded-3xl h-40 w-80 flex justify-center items-center'>
             <button className='text-5xl rounded-2xl bg-red-500 ml-6 px-4 pb-2'
             onClick={()=>{
               setCount(count - 1)
