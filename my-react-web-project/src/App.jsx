1c3a3daad8e7be8344b5e864a1205e5b863a3e84
Noman Jatala <cctv2093@gmail.com>
2025-08-13 16:29:53 +0500

    .

--- my-react-web-project/src/App.jsx
+++ my-react-web-project/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import Nav from './components/nav'
+import Hero from './components/hero'
+import Cards from './components/cards'
+import Animations from './animations'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <Nav />
+      <Nav />
+      <Hero />
+      <Cards />
+      <Animations/>
     </>
   )
 }
--- my-react-web-project/src/components/nav.jsx
+++ my-react-web-project/src/components/nav.jsx
@@ -1,7 +1,14 @@
 
 export const Nav = () => {
   return (
-    <div>nasdsdsdv</div>
+    <div className="bg-gray-800 text-white p-4 flex justify-between ">
+      <h1 className="text-lg font-bold">My Website</h1>
+      <ul className="flex space-x-4 flex-row">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
   )
 }
 export default Nav;
