e37416b83fea3f00ef1bdeb4ac86e7491fb59c61
Noman Jatala <cctv2093@gmail.com>
2025-08-13 16:00:02 +0500

    .

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -5,12 +5,12 @@ function App() {
 
   return (
     <>
-      <div className={`border-8 border-amber-400 h-screen flex justify-center flex-col items-center`}>
+      <div className={` ${count ? 'bg-amber-200' : 'bg-amber-100'} h-screen flex justify-center flex-col items-center`}>
         <div className='border-3 border-amber-300 flex flex-col justify-center items-center gap-4 p-4 bg-amber-100 rounded-lg shadow-xl'>
           <div className='font-bold text-xl'>Bg changer</div>
           <button className='bg-amber-400 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-amber-500'
+          onClick={() => setCount(!count)}
           >Change</button>
-          
           <div className='text-amber-900 font-semibold text-lg'>Click the button to change the background color</div>
           
         </div>
